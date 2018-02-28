86613699a226f14a9e47a577372e728ff48e616c
Peter Lotts <pml43@cam.ac.uk>
2018-02-28 11:15:41 +0000

    Add tests for RbtTrieNode matching key suffixes

--- test/RbtTrieNodeTest.hpp
+++ test/RbtTrieNodeTest.hpp
@@ -11,7 +11,7 @@
 class RbtTrieNodeTest : public CxxTest::TestSuite
 {
 public:
-	void x()
+	void x(void)
 	{
 		char k[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890./+=";
 		RbtTrieNode<char, int> tn(k, 65, NULL);
@@ -19,5 +19,28 @@ public:
 	void testNodeSuffixLengthLimit(void)
 	{
 		TS_ASSERT_THROWS(x(), std::out_of_range);
+	}
+
+	void testKeySuffixMatchTrue(void)
+	{
+		char k = 'A';
+		RbtTrieNode<char, int> tn(&k, 1, (int *)NULL);
+		TS_ASSERT(tn.matchesKeySuffix(&k, 1));
+	}
+
+	void testKeySuffixMatchMismatch(void)
+	{
+		char k = 'A';
+		RbtTrieNode<char, int> tn(&k, 1, (int *)NULL);
+		k = 'Q';
+		TS_ASSERT(!tn.matchesKeySuffix(&k, 1));
+	}
+
+	void testKeySuffixMatchDifferentLength(void)
+	{
+		char k = 'A';
+		RbtTrieNode<char, int> tn(&k, 1, (int *)NULL);
+		char j[] = "ABC";
+		TS_ASSERT(!tn.matchesKeySuffix(j, 3));
 	}
 };
