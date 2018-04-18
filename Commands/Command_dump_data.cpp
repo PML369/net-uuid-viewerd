b34ca66d599e9772251d7e48ae9c83c6f2b4b45b
Peter Lotts <pml43@cam.ac.uk>
2018-04-19 00:31:58 +0100

    Use a temporary vector to iterate over packets in command dump_data

--- Commands/Command_dump_data.cpp
+++ Commands/Command_dump_data.cpp
@@ -1,39 +1,7 @@
 #include "CommandTypes.hpp"
 #include "NetUuidData.hpp"
 
-#include <iterator>
-
-class AppendPacketDataToString
-{
-public:
-	struct output_iterator_tag {};
-
-	AppendPacketDataToString(std::string &string)
-	{
-		output = &string;
-	}
-
-	AppendPacketDataToString &operator++(void)
-	{
-		(*output) += current;
-		current.empty();
-		return *this;
-	}
-	AppendPacketDataToString operator++(AppendPacketDataToString &it)
-	{
-		output += current;
-		current.empty();
-		return *this;
-	}
-
-	void operator*(RbtTrie<char, PacketData>::tKey key)
-	{
-		current(key.first, key.second);
-	}
-private:
-	std::string *output;
-	std::string current;
-}
+#include <vector>
 
 std::string
 Command_dump_data::getCommand()
@@ -45,10 +13,43 @@ void
 Command_dump_data::execute(std::stringstream& sstream, std::string& out,
 				NetUuidData *data)
 {
-	AppendPacketDataToString it(out);
-	data->uuidTrie.getKeysWithPrefix(NULL, 0, it);
+	std::vector<PacketInfo *> allPackets;
+	data->uuidTrie.getValuesWithKeyPrefix(NULL, 0,
+				std::back_inserter(allPackets));
 
 	out += "Hello world: \n";
-	out += sstream.str();
+	for (std::vector<PacketInfo *>::iterator it = allPackets.begin();
+			it != allPackets.end(); ++it)
+	{
+		PacketInfo *p = *it;
+		out += "[uuid=";
+		out.append(p->uuid, 36);
+		if (p->hasTimeCreated)
+		{
+			out += ", created=";
+			out += std::to_string(p->timeCreated);
+		}
+		if (p->hasTimeDeleted)
+		{
+			out += ", deleted=";
+			out += std::to_string(p->timeDeleted);
+		}
+		out += ", address=[";
+		bool doneFirst = false;
+		char addrbuf[sizeof(void*) * 2 + 3];
+		for (std::vector<void *>::iterator itp =
+				p->addresses.begin();
+				itp != p->addresses.end(); ++itp)
+		{
+			if (doneFirst)
+			{
+				out += ", ";
+				doneFirst = true;
+			}
+			snprintf(addrbuf, sizeof(void*) * 2 + 3, "%p", *itp);
+			out.append(addrbuf);
+		}
+		out += "]]\n";
+	}
 	out += "\n";
 }
