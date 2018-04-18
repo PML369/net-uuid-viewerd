504877a151365aba46b46467370070c72f985f9b
Peter Lotts <pml43@cam.ac.uk>
2018-04-18 22:38:44 +0100

    Attempt to make an iterator to append values to a string - about to refactor into a vector iterator

--- Commands/Command_dump_data.cpp
+++ Commands/Command_dump_data.cpp
@@ -1,6 +1,40 @@
 #include "CommandTypes.hpp"
 #include "NetUuidData.hpp"
 
+#include <iterator>
+
+class AppendPacketDataToString
+{
+public:
+	struct output_iterator_tag {};
+
+	AppendPacketDataToString(std::string &string)
+	{
+		output = &string;
+	}
+
+	AppendPacketDataToString &operator++(void)
+	{
+		(*output) += current;
+		current.empty();
+		return *this;
+	}
+	AppendPacketDataToString operator++(AppendPacketDataToString &it)
+	{
+		output += current;
+		current.empty();
+		return *this;
+	}
+
+	void operator*(RbtTrie<char, PacketData>::tKey key)
+	{
+		current(key.first, key.second);
+	}
+private:
+	std::string *output;
+	std::string current;
+}
+
 std::string
 Command_dump_data::getCommand()
 {
@@ -11,6 +45,9 @@ void
 Command_dump_data::execute(std::stringstream& sstream, std::string& out,
 				NetUuidData *data)
 {
+	AppendPacketDataToString it(out);
+	data->uuidTrie.getKeysWithPrefix(NULL, 0, it);
+
 	out += "Hello world: \n";
 	out += sstream.str();
 	out += "\n";
